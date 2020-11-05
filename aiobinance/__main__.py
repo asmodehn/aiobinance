c301d06488b02ba81d45cbf942c971246f617727
AlexV <asmodehn@gmail.com>
2020-11-05 11:09:18 +0100

    new main strucutre and non working repl just yet. committing for code cleanup.

--- aiobinance/__main__.py
+++ aiobinance/__main__.py
@@ -28,53 +28,240 @@
 # => repl should provide access to functions to enable webconnections (authentication details)
 # => __main__ should provide long arguments to allow webconnections (authentication details)
 # => webconnection should not be possible without authentication (given exchange account details is stored in running process)
+from datetime import date, datetime
 
-
+import click
 
 import aiobinance.binance as binance
+import aiobinance.hummingbot as hummingbot
+import aiobinance.repl as repl
+from aiobinance._cli_params import Date
 
 # TODO
 # binance.trades_from_binance()
 
-import aiobinance.csv as csv
-import aiobinance.binance as binance
-import aiobinance.repl as repl
 
-import click
+@click.group()
+def cli():
+    pass
 
 
-@click.command()
-@click.argument('filename', type=click.Path(exists=True), required=False)
-def entrypoint(filename):
+@cli.command()
+@click.argument("filename", type=click.Path(exists=True), required=False)
+@click.pass_context
+def hummingbot(ctx, filename):
+    "provide a report of hummingbot trades"
 
-    if filename is None:  # repl mode
-        repl.embedded_repl()
+    trades = hummingbot.trades_from_csv(click.format_filename(filename))
+    print(trades.head())
 
-    else:  # report mode
-        trades = csv.trades_from_csv(click.format_filename(filename))
-        print(trades.head())
+    base = trades.Base.unique()
+    quote = trades.Quote.unique()
 
-        base = trades.Base.unique()
-        quote = trades.Quote.unique()
+    symbol = base[0] + quote[0]  # assuming only one value
 
-        symbol = base[0] + quote[0]  # assuming only one value
+    print(symbol)
 
-        print(symbol)
+    ohlcv = binance.price_from_binance(
+        start_time=trades.datetime.iloc[0].timestamp(),
+        end_time=trades.datetime.iloc[-1].timestamp(),
+        symbol=symbol,
+    )
 
-        ohlcv = binance.ohlcv_from_binance(
-            startTime=trades.Timestamp.iloc[0],
-            endTime=trades.Timestamp.iloc[-1],
-            symbol=symbol
+    import aiobinance.web
+
+    report = aiobinance.web.trades_layout(ohlcv=ohlcv, trades=trades)
+
+    from bokeh.io import output_file
+
+    output_file(f"{filename}_report.html")
+
+    from bokeh.plotting import show
+
+    show(report)
+
+
+@cli.command()
+@click.option("--verbose", default=False, is_flag=True)
+@click.pass_context
+def auth(ctx, verbose):
+    """ simple command to verify auth credentials and optionally store them. """
+    from aiobinance.config import (
+        BINANCE_API_KEYFILE,
+        load_api_keyfile,
+        save_api_keyfile,
+    )
+
+    # tentative loading of the API key
+    keystruct = load_api_keyfile()
+
+    if keystruct is None:
+        # no keyfile found
+        print(f"{BINANCE_API_KEYFILE} not found !")
+        # TODO : check for interactive terminal...
+        apikey = input("APIkey: ")
+        secret = input("secret: ")
+        store = input(f"Store it in {BINANCE_API_KEYFILE} [Y/n] ? ")
+        if not store:
+            store = "Y"
+        if store in ["Y", "y"]:
+            keystruct = save_api_keyfile(apikey=apikey, secret=secret)
+        else:
+            keystruct = {"key": apikey, "secret": secret}
+
+    # modifying parent context if present (to return)
+    if verbose:
+        print(
+            f"apikey and secret stored in {BINANCE_API_KEYFILE}.\nRemove it and re-run this command to replace it."
         )
+    if ctx.parent:
+        ctx.parent.params["apikey"] = keystruct.get("key")
+        ctx.parent.params["secret"] = keystruct.get("secret")
 
+        if keystruct:
+            print(f"apikey: {keystruct.get('key')}")
+        else:  # should come from context
+            print(f"apikey: {ctx.apikey}")
+
+    return 0  # exit status code
+
+
+@cli.command()
+@click.option("--apikey", default=None)
+@click.option("--secret", default=None)
+@click.pass_context
+def balance(ctx, apikey, secret):
+    """ retrieve balance for an authentified user"""
+
+    if apikey is None or secret is None:
+        ctx.invoke(auth, verbose=False)  # this should fill up arguments
+        apikey = ctx.params.get("apikey")
+        secret = ctx.params.get("secret")
+
+    # we always have the key here (otherwise it has been stored already)
+    binance.balance_from_binance(key=apikey, secret=secret)
+
+
+def positions():
+    """display existing positions of the authenticated user"""
+    raise NotImplementedError
+
+
+### COMMAND that rely on some timeframe ###
+
+
+@click.argument("market_pair", required=True)
+@click.option(
+    "--from", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to yesterday
+@click.option(
+    "--to", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to today
+@click.option("--html", default=False, is_flag=True)
+@click.pass_context
+def price(
+    ctx, market_pair, from_date: datetime, to_date: datetime, interval: str, html=True
+):
+
+    ohlcv = binance.price_from_binance(
+        symbol=market_pair,
+        start_time=from_date.timestamp(),
+        end_time=to_date.timestamp(),
+        interval=interval,
+    )
+
+    if html:
         import aiobinance.web
-        report = aiobinance.web.trades_layout(ohlcv=ohlcv, trades=trades)
+
+        report = aiobinance.web.price_plot(ohlcv=ohlcv)
 
         from bokeh.io import output_file
-        output_file("report.html")
+
+        output_file(f"{market_pair}_{from_date}_{to_date}_price.html")
 
         from bokeh.plotting import show
+
         show(report)
+    else:
+        # TODO : some kind of terminal plot...
+        raise NotImplementedError
 
 
-entrypoint()
+@click.argument("market_pair", required=False)
+@click.option(
+    "--from", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to yesterday
+@click.option(
+    "--to", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to today
+@click.option("--html", default=False, is_flag=True)
+@click.pass_context
+def trades(
+    ctx, market_pair, from_date: datetime, to_date: datetime, interval: str, html=True
+):
+    "display trades"
+    raise NotImplementedError
+
+
+### GROUP by human concerns over long time: daily, weekly, monthly ###
+
+
+@cli.group()
+@click.argument("market_pair", required=True)
+@click.option(
+    "--from", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to yesterday
+@click.option(
+    "--to", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to today
+@click.option("--html", default=False, is_flag=True)
+@click.pass_context
+def daily(ctx, market_pair, from_date, to_date, html):
+    """display OHLC"""
+
+    # compute interval to fit one request data into one day
+    ohlc(market_pair, from_date, to_date, interval, html=True)
+
+
+@cli.group()
+@click.argument("market_pair", required=True)
+@click.option(
+    "--from", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to last week
+@click.option(
+    "--to", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to today
+@click.option("--html", default=False, is_flag=True)
+@click.pass_context
+def weekly(ctx, market_pair, from_date, to_date, html):
+    """display OHLC"""
+    # compute interval to fit one request data into one week
+    ohlc(market_pair, from_date, to_date, interval, html=True)
+
+
+@cli.group()
+@click.argument("market_pair", required=True)
+@click.option(
+    "--from", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to lst month
+@click.option(
+    "--to", type=Date(formats=["%Y-%m-%d"]), default=str(date.today())
+)  # default to today
+@click.option("--html", default=False, is_flag=True)
+@click.pass_context
+def monthly(ctx, market_pair, from_date, to_date, html):
+    """display OHLC"""
+
+    # compute interval to fit one request data into one month
+    ohlc(market_pair, from_date, to_date, interval, html=True)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        # we use click to run a simple command -> result
+        cli()
+    else:
+        # or we go full interactive mode (no args)
+        repl.embedded_repl()
--- aiobinance/repl.py
+++ aiobinance/repl.py
@@ -1,4 +1,3 @@
-
 import IPython
 
 
@@ -6,11 +5,28 @@ def embedded_repl():
     # What is visible in this context controls
     # what is directly visible & actionable via the REPL.
 
-    import aiobinance
+    # First create a config object from the traitlets library
+    from traitlets.config import Config
 
-    return IPython.embed(
+    c = Config()
 
-    )
+    # Now we can set options as we would in a config file:
+    #   c.Class.config_value = value
+    # For example, we can set the exec_lines option of the InteractiveShellApp
+    # class to run some code when the IPython REPL starts
+    c.InteractiveShellApp.exec_lines = [
+        'print("\\nimporting aiokraken...\\n")',
+        "import aiokraken",
+        "aiokraken",
+    ]
+    c.InteractiveShell.colors = "LightBG"
+    c.InteractiveShell.confirm_exit = False
+    c.TerminalIPythonApp.display_banner = False
+
+    # TODO : %autoawait to easily run requests
+
+    # Now we start ipython, embedded, with our configuration
+    return IPython.embed(config=c)
 
 
 if __name__ == "__main__":
