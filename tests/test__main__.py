c0e999d27e0c7ca099f582c76e17e2c33f6c8032
AlexV <asmodehn@gmail.com>
2020-11-10 10:13:35 +0100

    restructuring test__main__.py to fail with message on cli run.

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -27,34 +27,36 @@ conf_pure_mm_coti_bnb_binance.yml,pure_market_making,binance,1598526454743,COTIB
     runner = CliRunner()
     result = runner.invoke(cli, f"hummingbot {fp.name}".split(), input="2")
 
-    assert result.exit_code == 0
-    assert (
-        "|    | time                             | symbol   |     id |    price |   qty |   quote_qty |   commission | commission_asset   | is_buyer   | is_maker   | order_id   | order_list_id   | is_best_match   |"
-        in result.output
-    )
-    assert (
-        "|  0 | 2020-08-27 10:32:20.551000+00:00 | COTIBNB  | 299167 | 0.003219 |   300 |      0.9657 |    0.0009657 | BNB                | True       | True       |            |                 |                 |"
-        in result.output
-    )
-    assert (
-        "|  1 | 2020-08-27 10:57:44.613000+00:00 | COTIBNB  | 299229 | 0.003261 |   300 |      0.9783 |    0.0009783 | BNB                | False      | True       |            |                 |                 |"
-        in result.output
-    )
-    assert (
-        "|  2 | 2020-08-27 11:07:34.594000+00:00 | COTIBNB  | 299244 | 0.003289 |   300 |      0.9867 |    0.0009867 | BNB                | False      | True       |            |                 |                 |"
-        in result.output
-    )
-    assert (
-        "|  3 | 2020-08-27 11:07:34.743000+00:00 | COTIBNB  | 299246 | 0.00329  |   300 |      0.987  |    0.000987  | BNB                | False      | True       |            |                 |                 |"
-        in result.output
-    )
+    if result.exit_code == 0:
+        assert (
+            "|    | time                             | symbol   |     id |    price |   qty |   quote_qty |   commission | commission_asset   | is_buyer   | is_maker   | order_id   | order_list_id   | is_best_match   |"
+            in result.output
+        )
+        assert (
+            "|  0 | 2020-08-27 10:32:20.551000+00:00 | COTIBNB  | 299167 | 0.003219 |   300 |      0.9657 |    0.0009657 | BNB                | True       | True       |            |                 |                 |"
+            in result.output
+        )
+        assert (
+            "|  1 | 2020-08-27 10:57:44.613000+00:00 | COTIBNB  | 299229 | 0.003261 |   300 |      0.9783 |    0.0009783 | BNB                | False      | True       |            |                 |                 |"
+            in result.output
+        )
+        assert (
+            "|  2 | 2020-08-27 11:07:34.594000+00:00 | COTIBNB  | 299244 | 0.003289 |   300 |      0.9867 |    0.0009867 | BNB                | False      | True       |            |                 |                 |"
+            in result.output
+        )
+        assert (
+            "|  3 | 2020-08-27 11:07:34.743000+00:00 | COTIBNB  | 299246 | 0.00329  |   300 |      0.987  |    0.000987  | BNB                | False      | True       |            |                 |                 |"
+            in result.output
+        )
+    else:
+        raise pytest.fail(msg=result.output, pytrace=False)
 
 
 def test_auth():
     """ testing auth command depending on user's environment """
     runner = CliRunner()
     result = runner.invoke(cli, "auth".split(), input="2")
-    # Result depends on the environment of the user running the tests
+    # Result and output depends on the environment of the user running the tests
     if os.path.exists(BINANCE_API_KEYFILE):
         assert result.exit_code == 0
         assert "apikey: " in result.output
@@ -68,7 +70,7 @@ def test_auth_verbose():
     """ testing auth --verbose command depending on user's environment """
     runner = CliRunner()
     result = runner.invoke(cli, "auth --verbose".split(), input="2")
-    # Result depends on the environment of the user running the tests
+    # Result and output depends on the environment of the user running the tests
     if os.path.exists(BINANCE_API_KEYFILE):
         assert result.exit_code == 0
         assert f"{BINANCE_API_KEYFILE}" in result.output
@@ -94,24 +96,28 @@ def test_balance(keyfile):
         input="2",
     )
 
-    assert result.exit_code == 0
-    assert "accountType: SPOT" in result.output
-    assert "canTrade: True" in result.output
-    assert "canWithdraw: True" in result.output
-    assert "canDeposit: True" in result.output
+    if result.exit_code == 0:
+        assert "accountType: SPOT" in result.output
+        assert "canTrade: True" in result.output
+        assert "canWithdraw: True" in result.output
+        assert "canDeposit: True" in result.output
 
-    assert "balances:" in result.output
-    assert " - 0.02084500 ETH (free: 0.02084500, locked: 0E-8)" in result.output
-    assert " - 12.83529493 BNB (free: 12.83529493, locked: 0E-8)" in result.output
-    assert " - 86.40000000 XRP (free: 86.40000000, locked: 0E-8)" in result.output
-    assert " - 208.93330180 EUR (free: 208.93330180, locked: 0E-8)" in result.output
-    assert " - 416.00000000 COTI (free: 416.00000000, locked: 0E-8)" in result.output
-    assert " - 0.94611754 SXP (free: 0.94611754, locked: 0E-8)" in result.output
+        assert "balances:" in result.output
+        assert " - 0.02084500 ETH (free: 0.02084500, locked: 0E-8)" in result.output
+        assert " - 12.83529493 BNB (free: 12.83529493, locked: 0E-8)" in result.output
+        assert " - 86.40000000 XRP (free: 86.40000000, locked: 0E-8)" in result.output
+        assert " - 208.93330180 EUR (free: 208.93330180, locked: 0E-8)" in result.output
+        assert (
+            " - 416.00000000 COTI (free: 416.00000000, locked: 0E-8)" in result.output
+        )
+        assert " - 0.94611754 SXP (free: 0.94611754, locked: 0E-8)" in result.output
 
-    assert "updateTime: 2020-11-02 20:15:07.791000+00:00" in result.output
-    assert "permissions: ['SPOT']" in result.output
-    assert "makerCommission: 10" in result.output
-    assert "takerCommission: 10" in result.output
+        assert "updateTime: 2020-11-02 20:15:07.791000+00:00" in result.output
+        assert "permissions: ['SPOT']" in result.output
+        assert "makerCommission: 10" in result.output
+        assert "takerCommission: 10" in result.output
+    else:
+        raise pytest.fail(msg=result.output, pytrace=False)
 
 
 @pytest.mark.vcr(
@@ -130,47 +136,49 @@ def test_trades(keyfile):
         input="2",
     )
 
-    assert result.exit_code == 0
-    assert (
-        "|    | time                             | symbol   |     id |    price |   qty |   quote_qty |   commission | commission_asset   | is_buyer   | is_maker   |   order_id |   order_list_id | is_best_match   |"
-        in result.output
-    )
-    assert (
-        "|  0 | 2020-08-27 10:32:20.409000+00:00 | COTIBNB  | 299167 | 0.003219 |   300 |    0.9657   |   0.00065495 | BNB                | True       | True       |   18441464 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  1 | 2020-08-27 10:57:44.478000+00:00 | COTIBNB  | 299229 | 0.003261 |   300 |    0.9783   |   0.00066035 | BNB                | False      | True       |   18443055 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  2 | 2020-08-27 11:07:34.459000+00:00 | COTIBNB  | 299244 | 0.003289 |   300 |    0.9867   |   0.00066602 | BNB                | False      | True       |   18446230 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  3 | 2020-08-27 11:07:34.459000+00:00 | COTIBNB  | 299246 | 0.00329  |   300 |    0.987    |   0.00066622 | BNB                | False      | True       |   18441463 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  4 | 2020-08-27 11:23:34.060000+00:00 | COTIBNB  | 299456 | 0.003285 |   300 |    0.9855   |   0.00068733 | BNB                | True       | True       |   18448574 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  5 | 2020-08-27 11:37:23.670000+00:00 | COTIBNB  | 299815 | 0.003245 |   300 |    0.9735   |   0.00065593 | BNB                | True       | True       |   18454107 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  6 | 2020-08-27 13:41:13.140000+00:00 | COTIBNB  | 300255 | 0.003209 |    61 |    0.195749 |   0.0001322  | BNB                | True       | True       |   18478551 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  7 | 2020-08-27 13:41:22.243000+00:00 | COTIBNB  | 300256 | 0.003209 |    97 |    0.311273 |   0.00021023 | BNB                | True       | True       |   18478551 |              -1 | True            |"
-        in result.output
-    )
-    assert (
-        "|  8 | 2020-08-27 17:06:47.354000+00:00 | COTIBNB  | 300607 | 0.003125 |   300 |    0.9375   |   0.00063539 | BNB                | True       | True       |   18506959 |              -1 | True            |"
-        in result.output
-    )
+    if result.exit_code == 0:
+        assert (
+            "|    | time                             | symbol   |     id |    price |   qty |   quote_qty |   commission | commission_asset   | is_buyer   | is_maker   |   order_id |   order_list_id | is_best_match   |"
+            in result.output
+        )
+        assert (
+            "|  0 | 2020-08-27 10:32:20.409000+00:00 | COTIBNB  | 299167 | 0.003219 |   300 |    0.9657   |   0.00065495 | BNB                | True       | True       |   18441464 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  1 | 2020-08-27 10:57:44.478000+00:00 | COTIBNB  | 299229 | 0.003261 |   300 |    0.9783   |   0.00066035 | BNB                | False      | True       |   18443055 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  2 | 2020-08-27 11:07:34.459000+00:00 | COTIBNB  | 299244 | 0.003289 |   300 |    0.9867   |   0.00066602 | BNB                | False      | True       |   18446230 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  3 | 2020-08-27 11:07:34.459000+00:00 | COTIBNB  | 299246 | 0.00329  |   300 |    0.987    |   0.00066622 | BNB                | False      | True       |   18441463 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  4 | 2020-08-27 11:23:34.060000+00:00 | COTIBNB  | 299456 | 0.003285 |   300 |    0.9855   |   0.00068733 | BNB                | True       | True       |   18448574 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  5 | 2020-08-27 11:37:23.670000+00:00 | COTIBNB  | 299815 | 0.003245 |   300 |    0.9735   |   0.00065593 | BNB                | True       | True       |   18454107 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  6 | 2020-08-27 13:41:13.140000+00:00 | COTIBNB  | 300255 | 0.003209 |    61 |    0.195749 |   0.0001322  | BNB                | True       | True       |   18478551 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  7 | 2020-08-27 13:41:22.243000+00:00 | COTIBNB  | 300256 | 0.003209 |    97 |    0.311273 |   0.00021023 | BNB                | True       | True       |   18478551 |              -1 | True            |"
+            in result.output
+        )
+        assert (
+            "|  8 | 2020-08-27 17:06:47.354000+00:00 | COTIBNB  | 300607 | 0.003125 |   300 |    0.9375   |   0.00063539 | BNB                | True       | True       |   18506959 |              -1 | True            |"
+            in result.output
+        )
+    else:
+        raise pytest.fail(msg=result.output, pytrace=False)
 
 
 @pytest.mark.vcr
@@ -188,20 +196,22 @@ def test_price(keyfile):
         input="2",
     )
 
-    assert result.exit_code == 0
-    assert (
-        "|     | open_time                 |     open |     high |      low |    close |   volume | close_time                       |        qav |   num_trades |   taker_base_vol |   taker_quote_vol |   is_best_match |"
-        in result.output
-    )
-    assert (
-        "|   0 | 2020-08-26 22:00:00+00:00 | 0.003472 | 0.003472 | 0.003468 | 0.003468 |     1962 | 2020-08-26 22:02:59.999000+00:00 |   6.8054   |            3 |                0 |          0        |               0 |"
-        in result.output
-    )
-    # testing first and last line only...
-    assert (
-        "| 480 | 2020-08-27 22:00:00+00:00 | 0.003153 | 0.003153 | 0.003153 | 0.003153 |        0 | 2020-08-27 22:02:59.999000+00:00 |   0        |            0 |                0 |          0        |               0 |"
-        in result.output
-    )
+    if result.exit_code == 0:
+        assert (
+            "|     | open_time                 |     open |     high |      low |    close |   volume | close_time                       |        qav |   num_trades |   taker_base_vol |   taker_quote_vol |   is_best_match |"
+            in result.output
+        )
+        assert (
+            "|   0 | 2020-08-26 22:00:00+00:00 | 0.003472 | 0.003472 | 0.003468 | 0.003468 |     1962 | 2020-08-26 22:02:59.999000+00:00 |   6.8054   |            3 |                0 |          0        |               0 |"
+            in result.output
+        )
+        # testing first and last line only...
+        assert (
+            "| 480 | 2020-08-27 22:00:00+00:00 | 0.003153 | 0.003153 | 0.003153 | 0.003153 |        0 | 2020-08-27 22:02:59.999000+00:00 |   0        |            0 |                0 |          0        |               0 |"
+            in result.output
+        )
+    else:
+        raise pytest.fail(msg=result.output, pytrace=False)
 
 
 if __name__ == "__main__":
