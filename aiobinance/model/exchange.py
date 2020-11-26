27a6135b81acff1223451816a6a415e83c5a214f
AlexV <asmodehn@gmail.com>
2020-11-26 17:02:46 +0100

    exchangeinfo model now relying on puremarket

--- aiobinance/model/exchange.py
+++ aiobinance/model/exchange.py
@@ -4,14 +4,8 @@ from typing import List, Optional
 
 from pydantic.dataclasses import dataclass
 
-
-@dataclass
-class Filter:
-
-    filter_type: str
-    min_price: Optional[Decimal] = None
-    max_price: Optional[Decimal] = None
-    tick_size: Optional[Decimal] = None
+from aiobinance.api.pure.filters import Filter
+from aiobinance.api.pure.puremarket import PureMarket
 
 
 @dataclass
@@ -22,32 +16,33 @@ class RateLimit:
     limit: int
 
 
-@dataclass
-class Symbol:
-
-    # TODO   a lot of str can be changed to enum and properly type checked.
-    symbol: str
-    status: str
-    base_asset: str
-    base_asset_precision: int
-    quote_asset: str
-    quote_precision: int
-    quote_asset_precision: int
-
-    base_commission_precision: int
-    quote_commission_precision: int
-
-    order_types: List[str]
-
-    iceberg_allowed: bool
-    oco_allowed: bool
-    is_spot_trading_allowed: bool
-    is_margin_trading_allowed: bool
-    quote_order_qty_market_allowed: bool
-
-    filters: List[Filter]
-
-    permissions: List[str]
+#  MVED TO API/PURE/MARKET
+# @dataclass
+# class Symbol:
+#
+#     # TODO   a lot of str can be changed to enum and properly type checked.
+#     symbol: str
+#     status: str
+#     base_asset: str
+#     base_asset_precision: int
+#     quote_asset: str
+#     quote_precision: int
+#     quote_asset_precision: int
+#
+#     base_commission_precision: int
+#     quote_commission_precision: int
+#
+#     order_types: List[str]
+#
+#     iceberg_allowed: bool
+#     oco_allowed: bool
+#     is_spot_trading_allowed: bool
+#     is_margin_trading_allowed: bool
+#     quote_order_qty_market_allowed: bool
+#
+#     filters: List[Filter]
+#
+#     permissions: List[str]
 
 
 # Leveraging pydantic to validate based on type hints
@@ -60,4 +55,4 @@ class Exchange:
 
     exchange_filters: List[Filter]
 
-    symbols: List[Symbol]
+    symbols: List[PureMarket]
