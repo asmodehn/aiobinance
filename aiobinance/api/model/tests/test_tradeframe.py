2c226b215b76fb506b361389f702907cf5baea82
AlexV <asmodehn@gmail.com>
2020-12-03 16:32:24 +0100

    adding frozen tradeframe to api.model

---
+++ aiobinance/api/model/tests/test_tradeframe.py
@@ -0,0 +1,113 @@
+import dataclasses
+import unittest
+
+import hypothesis.strategies as st
+from hypothesis import given
+
+from aiobinance.api.model.trade import Trade
+from aiobinance.api.model.tradeframe import TradeFrame
+
+
+# test TradeFrame (should have columns even empty)
+class TestTradeFrame(unittest.TestCase):
+    @given(tradeframe=TradeFrame.strategy())
+    def test_eq_sequence(self, tradeframe: TradeFrame):
+        assert tradeframe == tradeframe
+        # taking a copy via slice and comparing
+        assert tradeframe == tradeframe[:]
+
+    @given(tradeframe=TradeFrame.strategy(), data=st.data())
+    def test_index_sequence(self, tradeframe: TradeFrame, data):
+        # test we can iterate on contained data, and that length matches.
+        # Ref : https://docs.python.org/3/library/collections.abc.html
+        tfl = len(tradeframe)  # __len__
+
+        counter = 0
+        decount = -tfl
+        for t in tradeframe:  # __iter__
+            assert isinstance(t, Trade)
+            assert t in tradeframe  # __contains__
+            assert tradeframe[counter] == t  # __getitem__ on index in sequence
+            assert tradeframe[decount] == t  # __getitem on negative index in sequence
+            counter += 1
+            decount += 1
+
+        assert counter == tfl
+
+        with self.assertRaises(KeyError) as exc:
+            rid = data.draw(
+                st.one_of(
+                    st.integers(max_value=-len(tradeframe) - 1),
+                    st.integers(min_value=len(tradeframe)),
+                )
+            )
+            tradeframe[rid]
+        assert isinstance(exc.exception, KeyError)
+
+    @given(tradeframe=TradeFrame.strategy(), data=st.data())
+    def test_slice_sequence(self, tradeframe: TradeFrame, data):
+        # test we can iterate on contained data, and that length matches.
+        # Ref : https://docs.python.org/3/library/collections.abc.html
+        tfl = len(tradeframe)  # __len__
+
+        sss = data.draw(st.integers(min_value=-tfl, max_value=tfl))
+        if sss < 0:  # negative indexing on slices
+            s = slice(data.draw(st.integers(min_value=-tfl, max_value=sss)), sss)
+        else:
+            s = slice(sss, data.draw(st.integers(min_value=sss, max_value=tfl)))
+
+        tfs = tradeframe[s]
+        assert isinstance(tfs, TradeFrame)
+        assert (
+            len(tfs) == s.stop - s.start
+        ), f" len(tfs)!= s.stop - s.start : {len(tfs)} != {s.stop - s.start}"
+
+        counter = s.start
+        for t in tfs:  # __iter__
+            assert isinstance(t, Trade)
+            assert t in tradeframe  # value present in origin check
+            assert tradeframe[counter] == t  # same value as origin via equality check
+            counter += 1
+
+        assert counter - s.start == len(
+            tfs
+        ), f"counter-s.start != len(tfs) : {counter-s.start} != {len(tfs)}"
+
+        # test larger slice
+        s = slice(
+            data.draw(st.integers(max_value=-tfl)),
+            data.draw(st.integers(min_value=tfl)),
+        )
+        assert tradeframe[s] == tradeframe
+        # making sure we have the usual copy behavior when taking slices
+        assert tradeframe[s] is not tradeframe
+
+    @given(tradeframe=TradeFrame.strategy())
+    def test_str(self, tradeframe: TradeFrame):
+        # Check sensible information is displayed (order doesnt matter for output to human)
+
+        # we rely on optimize the tradeframe to drop compute details
+        # opttf = tradeframe.optimized()
+
+        tf_str = str(tradeframe)  # but we still call str on the frame
+        tf_header = tf_str.split("\n")[1]
+        for f in dataclasses.fields(Trade):
+            # check headers
+            assert f.name in tf_header
+        # TODO : build mapping for output comparison here ??
+
+        # TODO : compare values displayed (tricky)
+        # for t, s in zip(opttf.itertuples(), tf_str.split('\n')[3:-1]):
+        #     # check content by comparing with simplified data
+        #     for f in dataclasses.fields(Trade):
+        #         attr_val = getattr(t, f.name)
+        #         if attr_val is not None:  # optional attr case
+        #             assert str(attr_val) in s, f.name
+
+    # TODO
+    # @given(tradeframe=st_tradeframes())
+    # def test_optimized(self, tradeframe: TradeFrame):
+    #
+    #
+    #
+    #     raise NotImplementedError
---
+++ aiobinance/api/model/tradeframe.py
@@ -0,0 +1,167 @@
+from __future__ import annotations
+
+from dataclasses import asdict, dataclass, field, fields
+from datetime import datetime
+from decimal import Decimal, getcontext
+from typing import Iterable, List, Optional, Union
+
+import hypothesis.strategies as st
+import numpy as np
+import pandas as pd
+from cached_property import cached_property
+from hypothesis.strategies import composite
+from pydantic import validator
+
+# Leveraging pydantic to validate based on type hints
+from tabulate import tabulate
+
+from aiobinance.api.model.trade import Trade
+
+
+# Note : these are python dataclasses as pydantic cannot really typecheck dataframe content...
+@dataclass(frozen=True)
+class TradeFrame:
+    df: Optional[pd.DataFrame] = field(
+        init=True,
+        default=pd.DataFrame.from_records([], columns=[f.name for f in fields(Trade)]),
+    )
+
+    # @property
+    # def time(self) -> List[datetime]:
+    #     return self.df.time.to_list()
+    #
+    # @property
+    # def symbol(self) -> List[str]:  # TODO : improve
+    #     return self.df.symbol.to_list()
+
+    @cached_property
+    def id(self) -> List[int]:
+        return self.df.id.to_list()
+
+    #
+    # @property
+    # def price(self) -> List[Decimal]:
+    #     return self.df.price.to_list()
+    #
+    # @property
+    # def qty(self) -> List[Decimal]:
+    #     return self.df.qty.to_list()
+    #
+    # @property
+    # def quote_qty(self) -> List[Decimal]:
+    #     return self.df.quote_qty.to_list()
+    #
+    # @property
+    # def commission(self) -> List[Decimal]:
+    #     return self.df.commission.to_list()
+    #
+    # @property
+    # def commission_asset(self) -> List[str]:  # TODO : improve
+    #     return self.df.commission_asset.to_list()
+    #
+    # @property
+    # def is_buyer(self) -> List[bool]:
+    #     return self.df.is_buyer.to_list()
+    #
+    # @property
+    # def is_maker(self) -> List[bool]:
+    #     return self.df.is_maker.to_list()
+
+    # TODO
+    #  order_id: Optional[int]
+    #  order_list_id: Optional[int]
+    #  is_best_match: Optional[bool]
+
+    # Strategies, inferring attributes from type hints by default
+
+    @st.composite
+    @staticmethod
+    def strategy(draw, max_size=5):
+        tl = st.lists(
+            elements=Trade.strategy(), max_size=max_size, unique_by=lambda t: t.id
+        )
+        return TradeFrame.from_tradeslist(*draw(tl))
+
+    @classmethod
+    def from_tradeslist(cls, *trades: Trade):
+
+        df = pd.DataFrame.from_records(
+            [asdict(dc) for dc in trades], columns=[f.name for f in fields(Trade)]
+        )
+        return cls(df=df)
+
+    def __post_init__(self):
+        # Here we follow binance format and enforce proper python types
+        # TODO : assert proper dataframe format of columns...
+        pass
+
+    def __contains__(self, item: Trade):
+        for t in self:  # iterates and cast to Trade
+            if t == item:  # using Trade equality
+                return True
+        return False
+
+    def __eq__(self, other: TradeFrame) -> bool:
+        assert isinstance(
+            other, TradeFrame
+        )  # in our design the type infers the columns.
+        if self is other:
+            # here we follow python on equality https://docs.python.org/3.6/reference/expressions.html#id12
+            return True
+        elif len(self) == len(other):
+            # BEWARE : https://github.com/pandas-dev/pandas/issues/20442
+            for s, o in zip(self, other):  # this iterates and cast to Trade
+                if s != o:  # here we use Trade.__eq__ !
+                    break
+            else:
+                return True
+        else:
+            return False
+
+    def __getitem__(self, item: Union[int, slice]):
+        # in frozen tradeframe, index is equivalent to iloc in dataframe : position in the sequence
+
+        if isinstance(item, slice):
+            # dataframe slice handled by pandas for simplicity
+            tf = TradeFrame(df=self.df[item])
+            return tf
+
+        elif isinstance(item, int):
+            try:
+                return Trade(**self.df.iloc[item])
+            except IndexError as ie:
+                raise KeyError("TradeFrame index out of range") from ie
+        else:
+            raise KeyError(f"Invalid index {item}")
+
+    # NO SETTING ON TRADES :they are immutable events.
+
+    def __iter__(self):
+        for t in self.df.itertuples(index=False):
+            yield Trade(**t._asdict())
+
+    def __len__(self):
+        return len(self.df)
+
+    def __str__(self):
+        # optimize before display (high decimal precision is not manageable by humans)
+        optdf = self.optimized()
+        return tabulate(optdf, headers="keys", tablefmt="psql")
+
+    def optimized(
+        self,
+    ) -> pd.DataFrame:  # returns a raw DataFrame, containing numpy data, ready for fast compute.
+        """ optimize data for simplicity and speed, dropping precision."""
+        opt_copy = self.df.copy(deep=True)
+        opt_copy.convert_dtypes()
+        # also convert decimal to floats, losing precision
+        opt_copy.price = opt_copy.price.to_numpy("float64")
+        opt_copy.qty = opt_copy.qty.to_numpy("float64")
+        opt_copy.quote_qty = opt_copy.quote_qty.to_numpy("float64")
+        opt_copy.commission = opt_copy.commission.to_numpy("float64")
+        return opt_copy
+
+
+if __name__ == "__main__":
+
+    print(TradeFrame.strategy().example())
